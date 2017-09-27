e5a9f32f44036c5774bcc13bf01c2fc03d09a953
Lewis Mbogo <ogomb>
2017-09-27 09:59:22 +0300

    [ FEATURE ] Add routes file
    this file handles the diffrent routes in the application

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./containers/home";
+import NotFound from "./containers/notfound";
+import Login from "./containers/login";
+import Signup from "./containers/signup";
+import Logout from "./containers/logout";
+import AllBuckets from "./containers/allbuckets";
+
+export default () =>
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
+    <Route path="/signup" exact component={Signup} />
+    <Route path="/bucket" exact component={AllBuckets} />
+    <Route component={NotFound} />
+  </Switch>;
