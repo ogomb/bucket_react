2a7e0a0a2c2d7c088d370a1499fadbb829a38d02
Lewis Mbogo <ogomb>
2017-09-27 12:55:29 +0300

    [ FEATURE ] Add home.js
    shows the home component

---
+++ src/containers/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Bucket List</h1>
+          <p>A simple bucket list  app</p>
+        </div>
+      </div>
+    );
+  }
+}
