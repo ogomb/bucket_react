3e8771b0c25ee3ebce3c503a0cf5de359cccf7ec
Lewis Mbogo <ogomb>
2017-09-29 09:54:06 +0300

    [FEATURE] edit routes.js
    add change password to the routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Home from "./containers/home";
 import NotFound from "./containers/notfound";
 import Login from "./containers/login";
 import Signup from "./containers/signup";
+import ChangePassword from "./containers/changepassword";
+
 import AllBuckets from "./containers/allbuckets";
 
 export default () =>
@@ -11,6 +13,7 @@ export default () =>
     <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
     <Route path="/signup" exact component={Signup} />
+    <Route path="/changepassword" exact component={ChangePassword} />
     <Route path="/bucket" exact component={AllBuckets} />
     <Route component={NotFound} />
   </Switch>;
