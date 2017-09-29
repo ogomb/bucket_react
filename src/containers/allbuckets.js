c108b9f3babd96849c8cde6a6babfc521d162cd6
Lewis Mbogo <ogomb>
2017-09-29 09:44:09 +0300

    [BUGFIX] edit allbucket.js
     handle null values for the searched buckets

--- src/containers/allbuckets.js
+++ src/containers/allbuckets.js
@@ -4,6 +4,9 @@ import BucketAdd from './addbucket';
 import PreviewBucket from './previewbucket';
 import axios from 'axios';
 
+import { toast } from 'react-toastify';
+import CustomToast from "../components/customalerts";
+
 class AllBuckets extends Component {
   constructor(props){
     super(props);
@@ -31,12 +34,20 @@ onInputChange(term){
 
           })
         .then((response) => {
-          console.log('The response', response);
-          this.setState({searchedBuckets: response.data})
+
+          if(response.data.message){
+              toast.error("no buckets found");
+              this.setState({searchedBuckets: null});
+              console.log(this.state.searchedBuckets)
           }
-        )
+          else{
+            this.setState({searchedBuckets: response.data})
+          }
+        })
+
         .catch(error => {
           console.log(error);
+          this.setState({searchedBuckets: null});
 
         });
 }
@@ -70,7 +81,7 @@ onInputChange(term){
   render(){
     const {mybucketlists: allBuckets, selectedBucket, searchedBuckets } = this.state;
     return (
-       <div >
+       <div className="allbuckets">
        <div className="row">
        <div className="col-md-6">
         <BucketAdd onSearchTermChange={term => this.onInputChange(term)}/>
