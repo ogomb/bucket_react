f7209d0f818daf23e4529fb7ab3b3e58c6d95368
Lewis Mbogo <ogomb>
2017-10-02 10:18:42 +0300

    [FEATURE] edit signup_test.js
    add simulation test for the onchange field

--- src/__tests__/signup_test.js
+++ src/__tests__/signup_test.js
@@ -17,22 +17,35 @@ describe('Sign Page', () => {
     it('has username and password fields', ()=>{
       let input_username = wrapper.find("#username")
       expect(input_username.length).to.equal(1);
-      const mockUp = jest.fn();
-      expect(input_username.simulate('change', {mockUp}, {target: {value:"awesome"}}));
+      const target  ={
+        value : "awesome",
+        id : "username"
+      }
+
+      input_username.simulate('change', {target});
+      expect(wrapper.state().username).to.equal(target.value);
     });
 
     it('shows username is entered', () => {
       let input_password = wrapper.find("#password");
-      const mockUp = jest.fn();
-      expect(input_password.length).to.equal(1);
-      expect(input_password.simulate('change', {mockUp}, {target: {value: "awesome"}}));
+      const target  ={
+        value : "awesome",
+        id : "password"
+      }
+
+      input_password.simulate('change', {target});
+      expect(wrapper.state().password).to.equal(target.value);
     });
 
     it('shows email is entered', () => {
       let input_email = wrapper.find("#email");
-      const mockUp = jest.fn();
-      expect(input_email.length).to.equal(1);
-      expect(input_email.simulate('change', {mockUp}, {target: {value: "awesome@gmail.com"}}));
+      const target  ={
+        value : "awesome",
+        id : "email"
+      }
+
+      input_email.simulate('change', {target});
+      expect(wrapper.state().email).to.equal(target.value);
     });
 
   });
