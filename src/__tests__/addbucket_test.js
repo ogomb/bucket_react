9b654d8d9c09a13905b36a4762c7666c5eb1839a
Lewis Mbogo <ogomb>
2017-10-04 12:29:21 +0300

    [FEATURE] edit addbucket_test.js
    set up tests for addbucket_test component

--- src/__tests__/addbucket_test.js
+++ src/__tests__/addbucket_test.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import {configure, Enzyme, mount} from 'enzyme';
 import {expect }from 'chai';
-
-
+import {spy, sinon} from 'sinon';
+import moxios from 'moxios';
 
 import AddBucket from '../containers/addbucket';
+import AllBuckets  from '../containers/allbuckets';
 
 
 describe('AddBucket component ', () => {
     let wrapper;
+    let parent;
+    let parent2;
     beforeEach( () => {
-      wrapper = mount(<AddBucket />);
+      moxios.install();
+
+      parent= spy(AllBuckets.prototype,'onInputChange');
+      parent2 = spy(AllBuckets.prototype, 'handleSubmit');
+      wrapper = mount(<AddBucket onSearchTermChange={parent} onaddItem={parent2}/>);
+      //check allbuckets for the is loading state
+      wrapper.setState({isLoading: false});
+
+    });
+
+    afterEach(function () {
+      moxios.uninstall();
+      parent.restore();
+      parent2.restore();
     });
 
     it('has table element' ,() => {
@@ -27,7 +43,54 @@ describe('AddBucket component ', () => {
     it('has search input field', ()=>{
       let input_search = wrapper.find("#search_field")
       expect(input_search.length).to.equal(1);
+
+
+    moxios.wait(function () {
+     let request = moxios.requests.mostRecent()
+     request.respondWith({
+       status: 200,
+       response: ['id': fitem.id, 'name': fitem.name,'user_id': fitem.username
+       ]
+     }).then(function () {
+
+     });
     });
+
+    const target  = {
+      value : 3,
+      id : "edit"
+    }
+    input_search.simulate('change', {target});
+    expect(parent.called).to.equal(true);
+
+    });
+
+    it('has submit button for add bucket input field', ()=>{
+      let input_button = wrapper.find("#buttonsubmit")
+      expect(input_button.length).to.equal(1);
+
+
+    moxios.wait(function () {
+     let request = moxios.requests.mostRecent()
+     request.respondWith({
+       status: 200,
+       response: ['id': fitem.id, 'name': fitem.name,'user_id': fitem.username
+       ]
+     }).then(function () {
+
+     });
+    });
+
+    const target  = {
+      value : "awesome bucket",
+      id : "buttonsubmit"
+    }
+    input_button.simulate('submit', {target});
+    expect(parent2.called).to.equal(true);
+
+    });
+
+
 
 
   });
